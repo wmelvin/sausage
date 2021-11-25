9f20e1cc2eb726baae69bb69e5adcaa862d9919c
Bill Melvin <bill@billmelvin.com>
2021-11-25 13:09:28 -0500

    Write initial output.

--- sausage.py
+++ sausage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from collections import namedtuple
+from datetime import datetime
 from pathlib import Path
 
 import argparse
@@ -8,7 +9,7 @@ import subprocess
 import sys
 
 
-AppOptions = namedtuple("AppOptions", "help_cmd, target_file")
+AppOptions = namedtuple("AppOptions", "run_cmd, doc_path")
 
 
 def get_opts(argv) -> AppOptions:
@@ -19,29 +20,30 @@ def get_opts(argv) -> AppOptions:
     )
 
     ap.add_argument(
-        "help_cmd",
+        "run_cmd",
         action="store",
         help="Command to run to get help message from an application "
         + "(usually someapp -h).",
     )
 
     ap.add_argument(
-        "target_filename",
+        "doc_file",
         action="store",
         help="Name of the file in which to insert the help message text.",
     )
 
     args = ap.parse_args()
 
-    opts = AppOptions(args.help_cmd, args.target_filename)
+    p = Path(args.doc_file).expanduser().resolve()
+    assert p.exists()
 
-    assert Path(opts.target_file).exists()
+    opts = AppOptions(args.run_cmd, p)
 
     return opts
 
 
 def get_help_text(opts: AppOptions) -> str:
-    cmds = opts.help_cmd.split()
+    cmds = opts.run_cmd.split()
     try:
         result = subprocess.run(
             cmds,
@@ -60,7 +62,28 @@ def get_help_text(opts: AppOptions) -> str:
 def main(argv):
     opts = get_opts(argv)
 
-    print(f"BEGIN_HELP\n{get_help_text(opts)}\nEND_HELP")
+    help_text = get_help_text(opts)
+
+    assert "usage:" in help_text
+
+    # print(f"BEGIN_HELP\n{help_text}\nEND_HELP")
+
+    print(f"Reading '{opts.doc_path}'")
+
+    with open(opts.doc_path, "r") as f:
+        doc_lines = f.readlines()
+
+    ds = datetime.now().strftime("%Y%m%d_%H%M%S")
+    out_name = f"{opts.doc_path.stem}_{ds}{opts.doc_path.suffix}"
+    out_path = Path(opts.doc_path).parent.joinpath(out_name)
+
+    assert not out_path.exists()
+
+    print(f"Writing '{out_path}'")
+
+    with open(out_path, "w") as out_file:
+        for line in doc_lines:
+            out_file.write(f"{line}\n")
 
 
 if __name__ == "__main__":
