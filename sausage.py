0da20613ac6bf2d717b8dff2bbdc1a4289fb93ec
Bill Melvin <bill@billmelvin.com>
2021-11-25 20:16:30 -0500

    New function get_usage_section finds begin and end index in document lines.

--- sausage.py
+++ sausage.py
@@ -59,6 +59,50 @@ def get_help_text(opts: AppOptions) -> str:
     return s
 
 
+def get_usage_section(opts, doc_lines):
+    tbt = "```"
+    tbt_lines = []
+
+    usage_tag = f"usage: {Path(opts.run_cmd.split()[0]).stem}"
+    usage_lines = []
+
+    for ix, line in enumerate(doc_lines):
+        s = line.strip()
+        if s.startswith(tbt) and not (6 < len(s) and s.endswith(tbt)):
+            tbt_lines.append(ix)
+        if usage_tag in s.lower():
+            usage_lines.append(ix)
+
+    if 0 == len(usage_lines):
+        print(f"No reference to '{usage_tag}' found in document.")
+        print("Cannot proceed.")
+        sys.exit(1)
+
+    #  For this to work, there needs to be only one "usage:" reference
+    #  in the document.
+    if 1 < len(usage_lines):
+        print(f"More than one reference to '{usage_tag}' found in document.")
+        print("Cannot proceed.")
+        sys.exit(1)
+
+    tbt_before = -1
+    tbt_after = -1
+
+    for x in tbt_lines:
+        if x <= usage_lines[0]:
+            tbt_before = x
+        elif tbt_after < 0:
+            tbt_after = x
+
+    if tbt_before < 0 or tbt_after < 0:
+        print("Could not find surrounding triple-backticks to indicate code")
+        print("section for usage text.")
+        print("Cannot proceed.")
+        sys.exit(1)
+
+    return tbt_before, tbt_after
+
+
 def main(argv):
     opts = get_opts(argv)
 
@@ -74,16 +118,22 @@ def main(argv):
         doc_lines = f.readlines()
 
     ds = datetime.now().strftime("%Y%m%d_%H%M%S")
-    out_name = f"{opts.doc_path.stem}_{ds}{opts.doc_path.suffix}"
+    out_name = f"{opts.doc_path.stem}_MODIFIED_{ds}{opts.doc_path.suffix}"
     out_path = Path(opts.doc_path).parent.joinpath(out_name)
 
     assert not out_path.exists()
 
-    print(f"Writing '{out_path}'")
+    ix_before, ix_after = get_usage_section(opts, doc_lines)
 
+    a = doc_lines[:ix_before]
+    b = doc_lines[ix_after:]
+
+    out_lines = a + [t for t in help_text.split("\n")] + b
+
+    print(f"Writing '{out_path}'")
     with open(out_path, "w") as out_file:
-        for line in doc_lines:
-            out_file.write(f"{line}\n")
+        for s in out_lines:
+            out_file.write(f"{s}\n")
 
 
 if __name__ == "__main__":
